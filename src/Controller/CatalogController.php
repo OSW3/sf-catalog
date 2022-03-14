280317b131e014543548aeb53bd7c945d71f8bd8
Arnaud BODEL <contact@osw3.net>
2022-03-14 17:00:13 +0100

    -

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -2,16 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\ProductType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
+/**
+ * @Route("/product", name="app_catalog_")
+ * 
+ * site.com/products                Affiche la liste des produits
+ * site.com/product                 Créer un produit
+ * site.com/product/{id}            Affiche le détail d'un produit
+ * site.com/product/{id}/edit       Modifier un produit
+ * site.com/product/{id}/delete     Supprime un produit
+ */
 class CatalogController extends AbstractController
 {
     /** 
      * Affiche la liste des produits
      * 
-     * @Route("", name="")
+     * url ex: /products
+     * name: app_catalog_index
+     * 
+     * @Route("s", name="index")
      */
     public function index(): Response
     {
@@ -23,19 +39,69 @@ class CatalogController extends AbstractController
     /**
      * Ajoute un produit dans la BDD
      * 
-     * @Route("", name="")
+     * url ex: /product
+     * name: app_catalog_create
+     * 
+     * @Route("", name="create")
      */
-    public function create()
+    public function create(Request $request, ValidatorInterface $validator): Response
     {
+        $errors = [];
+
+        // Create Form
+        // --
+
+        // Initalisation du Produit
+        $product = new Product;
+
+        // Construction du formulaire
+        $form = $this->createForm(ProductType::class, $product);
+
+        // Handle the Request
+        $form->handleRequest($request);
+
+        // Catch form submission
+        if ( $form->isSubmitted() )
+        {
+            // Handle form errors
+            $errors = $validator->validate( $product );
+
+            // If the form is valid
+            if ( $form->isValid() )
+            {
+                
+                // Enregistrement en BDD
+                $em = $this->getDoctrine()->getManager();
+                $em->persist( $product );
+                $em->flush();
+
+                // Rerdirection de l'utilisateur
+                return $this->redirectToRoute('app_catalog_index');
+            }
+        }
+
+        // Préparation du formulaire pour la vue
+        $form = $form->createView();
+
+
+        // Response HTTP
+        // --
+
+        return $this->render('catalog/create.html.twig', [
+            'form' => $form
+        ]);
     }
 
 
     /**
      * Affiche le détail d'un produit
      * 
-     * @Route("", name="")
+     * url ex: /product/42
+     * name: app_catalog_read
+     * 
+     * @Route("/{id}", name="read")
      */
-    public function read()
+    public function read(int $id): Response
     {
     }
 
@@ -43,9 +109,12 @@ class CatalogController extends AbstractController
     /**
      * Modifier la fiche d'un produit
      * 
-     * @Route("", name="")
+     * url ex: /product
+     * name: app_catalog_update
+     * 
+     * @Route("/{id}/edit", name="update")
      */
-    public function update()
+    public function update(int $id): Response
     {
     }
 
@@ -53,9 +122,12 @@ class CatalogController extends AbstractController
     /**
      * Supprimer la fiche d'un produit
      * 
-     * @Route("", name="")
+     * url ex: /product
+     * name: app_catalog_delete
+     * 
+     * @Route("/{id}/delete", name="delete")
      */
-    public function delete()
+    public function delete(int $id): Response
     {
     }
 }
