726a8e81c066ece79be3514ea9cab8113314ab93
Arnaud BODEL <contact@osw3.net>
2022-03-14 14:34:34 +0100

    -

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -8,12 +8,54 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CatalogController extends AbstractController
 {
-    /**
-     * @Route("/nos-produits", name="app_catalog_index")
+    /** 
+     * Affiche la liste des produits
+     * 
+     * @Route("", name="")
      */
     public function index(): Response
     {
         return $this->render('catalog/index.html.twig', [
         ]);
+    }
+
+
+    /**
+     * Ajoute un produit dans la BDD
+     * 
+     * @Route("", name="")
+     */
+    public function create()
+    {
+    }
+
+
+    /**
+     * Affiche le détail d'un produit
+     * 
+     * @Route("", name="")
+     */
+    public function read()
+    {
+    }
+
+
+    /**
+     * Modifier la fiche d'un produit
+     * 
+     * @Route("", name="")
+     */
+    public function update()
+    {
+    }
+
+
+    /**
+     * Supprimer la fiche d'un produit
+     * 
+     * @Route("", name="")
+     */
+    public function delete()
+    {
     }
 }
