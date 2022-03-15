47046797ebffb66723f1465c63e924a8f2b2794d
Arnaud BODEL <contact@osw3.net>
2022-03-15 12:10:07 +0100

    -

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -9,6 +9,8 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ProductType extends AbstractType
 {
@@ -18,13 +20,50 @@ class ProductType extends AbstractType
 
             // Title
             ->add('title', TextType::class, [
-                
+
+                // label
+                'label' => "Nom du produit",
+                'label_attr' => [
+                    'class' => "col-sm-3 col-form-label",
+                ],
+
+                // make it required
+                'required' => true,
+
+                // Field attributes
+                'attr' => [
+                    'class' => "form-control",
+                    'placeholder' => "Saisir le nom du produit",
+                ],
+
+                // Field helper
+                'help' => "Merci de le nom du produit dans le champ",
+                'help_attr' => [
+                    'class' => "form-text text-muted",
+                ],
+
+                // Form constraints and error messages
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Le nom du produit est obligatoire",
+                    ]),
+                    new Length([
+                        'max' => 120,
+                        'maxMessage' => "Le nom du produit est limité à {{ limit }} caracteres",
+                    ]),
+                ],
+
             ])
 
             // Description
+            // label: Description du produit
+            // min : 10 caracteres
+            // max : 80 caracters
             ->add('description', TextareaType::class)
 
             // Price
+            // label: Prix du produit en dollar
+            // max : 999,99
             ->add('price', MoneyType::class)
 
         ;
