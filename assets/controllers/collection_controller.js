e81277bc370716fd336d90a7d9cd559938b67bfa
Arnaud BODEL <contact@osw3.net>
2022-03-21 11:39:54 +0100

    -

--- assets/controllers/collection_controller.js
+++ assets/controllers/collection_controller.js
@@ -2,26 +2,32 @@ import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
 
+    container;
     counter;
     newWidget;
     templateId;
 
     connect() {
 
-        let container = this.element;
-        this.counter = container.dataset.itemCounter;
-        this.templateId = container.dataset.templateId;
+        this.container = this.element;
+        this.counter = this.container.dataset.itemCounter;
+        this.templateId = this.container.dataset.templateId;
 
-        
-        this.newWidget = container.querySelector(`[id=${this.templateId}]`);
-        console.log( this.newWidget );
+        this.newWidget = this.container.querySelector(`[id=${this.templateId}]`);
 
-        // this.newWidget = document.getElementById(this.templateId);
-
-        console.log( this.newWidget );
     }
 
     add()
     {
+        // Remplacement de la chaine "__name__"
+        this.newWidget = this.newWidget.innerHTML.replace(/__name__/g, this.counter);
+
+        // Incrémentation du compteur
+        this.counter++;
+
+        // Mise à jour du compteur
+        this.container.dataset.counter = this.counter;
+
+        console.log( this.newWidget );
     }
 }
